46d35d2c1ceefbc9cb03e5c75532eb77b47a9810
Eryk Dobaj <dobajeryk@gmail.com>
2021-07-27 08:48:15 +0200

    Singleton - implementacja

--- src/singleton/przyklad/GameEngine.java
+++ src/singleton/przyklad/GameEngine.java
@@ -5,11 +5,25 @@ public class GameEngine {
     private int hp = 100;
     private String characterName = "Ton";
 
+    private static GameEngine instance;
+
+    private GameEngine() {
+
+    }
+
     public void run() {
         while (true) {
             //czekamy na input gracza
             //zmieniamy stan gry
             //renderujemy grafike
         }
+    }
+
+    public static GameEngine getInstance() {
+        if (instance == null) {
+            instance = new GameEngine();
+        }
+
+        return instance;
     }
 }
---
+++ src/singleton/przyklad/Main.java
@@ -0,0 +1,11 @@
+package singleton.przyklad;
+
+public class Main {
+    public static void main(String[] args) {
+//        GameEngine gameEngine = new GameEngine();
+        GameEngine engine1 = GameEngine.getInstance();
+        GameEngine engine2 = GameEngine.getInstance();
+
+        System.out.println(engine1 == engine2);
+    }
+}
