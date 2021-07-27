319a3d89f89c9b7de5e9206dafbd4634d585814d
Eryk Dobaj <dobajeryk@gmail.com>
2021-07-27 10:04:01 +0200

    Builder - wprowadznie

---
+++ src/builder/przyklad/Main.java
@@ -0,0 +1,10 @@
+package builder.przyklad;
+
+public class Main {
+    public static void main(String[] args) {
+        House house1 = new House("walls", "floors", "rooms", "windows", "doors", "garage");
+//        House house2 = new House("");
+
+
+    }
+}
