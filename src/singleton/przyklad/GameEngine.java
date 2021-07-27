979b387a35f02a925c50546f59d50d488c0effde
Eryk Dobaj <dobajeryk@gmail.com>
2021-07-27 08:44:50 +0200

    Singleton - wprowadzenie

---
+++ src/singleton/przyklad/GameEngine.java
@@ -0,0 +1,15 @@
+package singleton.przyklad;
+
+public class GameEngine {
+
+    private int hp = 100;
+    private String characterName = "Ton";
+
+    public void run() {
+        while (true) {
+            //czekamy na input gracza
+            //zmieniamy stan gry
+            //renderujemy grafike
+        }
+    }
+}
